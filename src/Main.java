138e423ef323b2f0b173a490ef6d0e1b52b23147
Thiago Oliveira <onthiagooliveira@outlook.com>
2025-10-11 06:43:39 -0300

    Primeiro programa java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("""
+                Jornada Backend Oracle Next Education,
+                 Início em Outubro de 2025.
+                """);
+    }
+}
